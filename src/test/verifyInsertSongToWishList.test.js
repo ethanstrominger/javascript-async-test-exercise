8117bd0018de8fcf32a99bb41c0c35f11239bc87
ethanstrominger <ethanstrominger2@gmail.com>
2019-06-25 11:14:54 -0400

    Created test to very insertSongToWishList returns a number (id of inserted record)

---
+++ src/test/verifyInsertSongToWishList.test.js
@@ -0,0 +1,10 @@
+import insertSongToWishList from '../insertSongToWishList';
+
+test('Verify inserting returns an id', signalEndOfTest => {
+  async function asynchTest() {
+    const id = await insertSongToWishList(84,"Respect",37,'Aretha Franklin');
+    expect(typeOf(id)).toBe("number")
+    signalEndOfTest();
+  }
+  asynchTest();
+});
