8b9cbb8ae0c9036d085aa1ab60245909540a89bf
ethanstrominger <ethanstrominger2@gmail.com>
2019-06-25 14:55:11 -0400

    Refactored from forEach to for loop to allow for break and added data to json file

--- src/getSongFromWishList.js
+++ src/getSongFromWishList.js
@@ -3,10 +3,12 @@ import getWishList from './getWishList';
 export default async function getSongFromWishList(WishListID, file) {
   let jsonArray = await getWishList(file);
   let retVal;
-  jsonArray.forEach(function(json) {
+  for (let i = 0; i < jsonArray.length; i++) {
+    let json = jsonArray[i];
     if (json.WishListID === WishListID) {
       retVal = json;
+      break;
     }
-  });
+  }
   return retVal;
 }
