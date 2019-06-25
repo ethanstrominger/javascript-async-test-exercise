737f35b7bc60a37f16c6d88c04cd58c066a60c37
ethanstrominger <ethanstrominger2@gmail.com>
2019-06-25 13:18:50 -0400

    Create test for getWishList, modified test for getSongFromWishList, created json file to use for source data

--- src/test/verifyGetWishList.test.js
+++ src/test/verifyGetWishList.test.js
@@ -1,13 +1,9 @@
-import getSongFromWishList from '../getSongFromWishList';
+import getWishList from '../getWishList';
 
-test('Verify get returns a json object', signalEndOfTest => {
+test('Verify getWishList returns a json array', signalEndOfTest => {
   async function asynchTest() {
-    const json = await getSongFromWishList(84);
-    expect(json.wishListID).toBeDefined();
-    expect(json.songID).toBeDefined();
-    expect(typeof json.songTitle).toBe('string');
-    expect(json.artistID).toBeDefined();
-    expect(json.artistName).toBeDefined();
+    const json = await getWishList(84);
+    expect(Array.isArray(json)).toBe(true);
     signalEndOfTest();
   }
   asynchTest();
@@ -15,7 +11,7 @@ test('Verify get returns a json object', signalEndOfTest => {
 
 // test('Verify get retrieves expected values', signalEndOfTest => {
 //   async function asynchTest() {
-//     const json = await getSongFromWishList(1, 'testWishList.json');
+//     const json = await getWishList(1, 'testWishList.json');
 //     expect(json.wishListID).toBe(1);
 //     expect(json.songID).toBe(32);
 //     expect(typeof json.songTitle).toBe('Respect');
